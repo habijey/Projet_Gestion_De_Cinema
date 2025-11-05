5e0ee34c03c26cf1e0012acb0b713b145786a21c
habijey <habilanjeyarasa@gmail.com>
2025-11-05 16:02:02 +0100

    Add files via upload

---
+++ classe.py
@@ -0,0 +1,7 @@
+class film:
+    def _init_(self,titre, duree, genre, realisateur):
+        self.titre = titre
+        self.duree = duree
+        self.genre = genre
+        self.realisateur = realisateur
+    
